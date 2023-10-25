07b6630be914a2d5d426dcc54fdefcb17239c861
Kourosh <102174082+kouroshAKiani@users.noreply.github.com>
2023-10-25 11:15:52 +0330

    Update compute_metrics.R

--- compute_metrics.R
+++ compute_metrics.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 source('F1-score.R')
 # Define function to evaluate models (accuracy, precision,...) with confidence intervals
 compute_metrics <- function(confusion_matrix) {
@@ -17,24 +16,3 @@ compute_metrics <- function(confusion_matrix) {
   )
   
   return(result)
-=======
-source('F1-score.R')
-# Define function to evaluate models (accuracy, precision,...) with confidence intervals
-compute_metrics <- function(confusion_matrix) {
-  accuracy <- BinomCI(sum(diag(confusion_matrix)), sum(confusion_matrix), conf.level = 0.95, method = "clopper-pearson")
-  precision <- BinomCI(confusion_matrix[2, 2], sum(confusion_matrix[, 2]), conf.level = 0.95, method = "clopper-pearson")
-  sensitivity <- BinomCI(confusion_matrix[2, 2], sum(confusion_matrix[2, ]), conf.level = 0.95, method = "clopper-pearson")
-  specificity <- BinomCI(confusion_matrix[1, 1], sum(confusion_matrix[1, ]), conf.level = 0.95, method = "clopper-pearson")
-  f1_score <- f1scores(confusion_matrix)$Confidence.Interval[2,]  
-  
-  result <- list(
-    Accuracy = accuracy,
-    Precision = precision,
-    Sensitivity = sensitivity,
-    Specificity = specificity,
-    F1_Score = f1_score
-  )
-  
-  return(result)
->>>>>>> c18c5be1391a6c1982e85029db52adc20aff265f
-}
