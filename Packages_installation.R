2afe5f328c309371d7e37455dbe555ccec47a9cd
Kourosh <102174082+kouroshAKiani@users.noreply.github.com>
2023-10-25 10:58:20 +0330

    Update Packages_installation.R

--- Packages_installation.R
+++ Packages_installation.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # package installation and loading code using the install_and_load_package function.
 install_and_load_package <- function(package) {
   if (!(package %in% installed.packages()[,"Package"])) {
@@ -18,25 +17,3 @@ required_packages <- c(
 
 for (package in required_packages) {
   install_and_load_package(package)
-=======
-# package installation and loading code using the install_and_load_package function.
-install_and_load_package <- function(package) {
-  if (!(package %in% installed.packages()[,"Package"])) {
-    install.packages(package)
-  }
-  library(package, character.only = TRUE)
-}
-
-required_packages <- c(
-  "readxl", "gbm", "DiagrammeR", "DiagrammeRsvg", "Rcpp", "rpart",
-  "rpart.plot", "randomForest", "corrplot", "ModelMetrics", "ROCR", "caret",
-  "mlbench", "kernlab", "e1071", "MASS", "klaR", "dplyr", "naivebayes",
-  "DescTools", "pROC", "rsample", "titanic", "funModeling", "GA", "DALEX", "optparse",
-  "plotly", "ggplot2", "smotefamily", "ingredients","r2d3","ROSE", "patchwork", "devtools", "ggpubr",
-  "catboost", "devEMF"
-)
-
-for (package in required_packages) {
-  install_and_load_package(package)
->>>>>>> c18c5be1391a6c1982e85029db52adc20aff265f
-}
